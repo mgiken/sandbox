b1e321e306246b593661adf32877532e4fe6cbcd
Ryuji Matsumura <ryuji@mgiken.com>
2011-11-10 11:41:59 +0900

    devpo

---
+++ devpo/devpo.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import render_template
+
+import asyncore
+import smtpd
+import time
+import os
+import thread
+
+DIR = '/tmp/devpo'
+if not os.path.isdir(DIR): os.makedirs(DIR)
+
+# smtpd ----------------------------------------------------------------------
+
+class DevelopmentSMTPServer(smtpd.SMTPServer):
+  def process_message(self, peer, mailfrom, rcpttos, data):
+    with open("%s/%s.txt" % (DIR, time.time()), 'w') as f:
+      f.write(data)
+
+def run_smtpd():
+  srv = DevelopmentSMTPServer(('127.0.0.1', 2525), None)
+  asyncore.loop()
+
+# httpd ----------------------------------------------------------------------
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  mails = sorted(os.listdir(DIR))
+  return render_template('index.html', mails=mails)
+
+@app.route('/<id>')
+def mail(id):
+  with open("%s/%s" % (DIR, id), 'rb') as f:
+    return f.read()
+
+if __name__ == '__main__':
+  thread.start_new_thread(run_smtpd, ())
+  app.run(host='0.0.0.0')
