bed0f95ff0d21acf71db428344fc7de85d732254
mitsu123 <mitsuhisa@gmail.com>
2011-12-03 23:51:23 +0900

    comp

--- techpi/techpi.pl
+++ techpi/techpi.pl
@@ -1,24 +1,15 @@
 #!/usr/bin/env perl
-use strict;use FindBin;use Encode;use Fileio;use WWW::Mechanize;use URI;
-use Web::Scraper;use XML::FeedPP;our $VERSION = '0.1';
-sub each_item(&$) {
-    my $items = scraper {
-        process '#news-items article', 'items[]' => scraper {
-            process 'h1 a', link => '@href', title => 'TEXT';
-            process 'time', 'pubDate' => '@datetime';
-            process '.tags li', 'category[]' => 'TEXT';
-        };
-    };
-    $_[0]->() for @{$items->scrape($_[1])->{items}};
-}
-my ($id, $pw) = readlines "$FindBin::Bin/user.txt";
-my $mech = WWW::Mechanize->new;
-$mech->get('https://techp.jp/login');
-$mech->submit_form(fields => {id => $id, pw => $pw});
+use FindBin;use Encode;use Fileio;use WWW::Mechanize;use URI;
+use Web::Scraper;use XML::FeedPP;our $VERSION='0.1';
+$items=scraper{process '#news-items article','items[]'=>scraper{process 'h1 a',
+link=>'@href',title=>'TEXT';process 'time','pubDate'=>'@datetime';process
+'.tags li','category[]'=>'TEXT'}};
+sub each_item(&$) {$_[0]->() for @{$items->scrape($_[1])->{items}}}
+($id,$pw)=readlines "$FindBin::Bin/user.txt";
+$mech=WWW::Mechanize->new;$mech->get('https://techp.jp/login');
+$mech->submit_form(fields=>{id=>$id,pw=>$pw});
 $mech->get('https://techp.jp/news');
-my $fpp = XML::FeedPP::RSS->new;
-$fpp->title('techp.jp/news');
-$fpp->link('https://techp.jp/news');
-$fpp->pubDate(time);
+$fpp=XML::FeedPP::RSS->new;$fpp->title('techp.jp/news');
+$fpp->link('https://techp.jp/news');$fpp->pubDate(time);
 each_item {$fpp->add_item(%$_)} $mech->content;
 print encode('utf8', $fpp->to_string);
--- techpo/techpo.pl
+++ techpo/techpo.pl
@@ -1,21 +1,12 @@
 #!/usr/bin/env perl
-use strict;use FindBin;use Fileio;use WWW::Mechanize;use URI;use Web::Scraper;
-our $VERSION = '0.1.1';
-sub each_item(&$) {
-    my $items = scraper {
-        process '.hotentry.main-entries .entry-body', 'items[]' => scraper {
-            process '.entry-link', url => '@href';
-            process '.entry-info .tags .tag', 'tags[]' => 'TEXT';
-        };
-    };
-    $_[0]->() for @{$items->scrape(URI->new($_[1]))->{items}};
-}
-my ($id, $pw) = readlines "$FindBin::Bin/user.txt";
-my $mech = WWW::Mechanize->new;
-$mech->get('https://techp.jp/login');
-$mech->submit_form(fields => {id => $id, pw => $pw});
+use FindBin;use Fileio;use WWW::Mechanize;use URI;use Web::Scraper;
+our $VERSION='0.1.2';$items=scraper{process '.hotentry.main-entries .entry-body'
+,'items[]'=>scraper{process '.entry-link',url=>'@href';process
+'.entry-info .tags .tag','tags[]'=>'TEXT'}};
+sub each_item(&$) {$_[0]->() for @{$items->scrape(URI->new($_[1]))->{items}}}
+($id,$pw)=readlines "$FindBin::Bin/user.txt";
+$mech=WWW::Mechanize->new;$mech->get('https://techp.jp/login');
+$mech->submit_form(fields=>{id=>$id,pw=>$pw});
 $mech->get('https://techp.jp/news/post?_='.time*1000);
-each_item {
-    $mech->submit_form(fields => {url => $_->{url}});
-    $mech->submit_form(fields => {tags => join(' ', @{$_->{tags}})});
-} 'http://b.hatena.ne.jp/hotentry/it';
+each_item {$mech->submit_form(fields=>{url=>$_->{url}});$mech->submit_form(
+fields=>{tags=>join(' ',@{$_->{tags}})})} 'http://b.hatena.ne.jp/hotentry/it';
